01907d9856e891cc6247fed5045cde1d15589e6e
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-12-01 02:48:05 +0100

    Format lua code

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 .PHONY: format
 format:
 	@find src -iname '*.hpp' -o -iname '*.h' -o -iname '*.cpp' | xargs clang-format-13 -i
+	@find src -iname '*.lua' | xargs lua-format -c .lua-format -i
 
 .PHONY: format-check
 format-check:
-	@find src -iname '*.hpp' -o -iname '*.h' -o -iname '*.cpp' | xargs -n 1 -P 1 -I{} -t sh -c 'clang-format-13 -style=file {} | diff - {}'
+	@find src -iname '*.hpp' -o -iname '*.h' -o -iname '*.cpp' | xargs -P 1 -I{} -t sh -c 'clang-format-13 -style=file {} | diff - {}'
+	@find src -iname '*.lua' | xargs -P 1 -I{} -t sh -c 'lua-format -c .lua-format {} | diff - {}'
--- src/scripts/demo_Components.lua
+++ src/scripts/demo_Components.lua
@@ -1,17 +1,3 @@
-local white = juce.Colour.new(255, 255, 255, 255)
-local black = juce.Colour.new(  0,   0,   0, 255)
-local red   = juce.Colour.new(255,   0,   0, 255)
-local green = juce.Colour.new(  0, 255,   0, 255)
-local blue  = juce.Colour.new(  0,   0, 255, 255)
-local grey  = juce.Colour.new( 25,  25,  25, 255)
-
-function randomColor ()
-  local r = random:nextInt(255)
-  local g = random:nextInt(255)
-  local b = random:nextInt(255)
-  return juce.Colour.new(r, g, b, 255);
-end
-
 local mainComponent = juce.LuaComponent.new()
 local btn_1 = juce.TextButton.new(juce.String.new("Foo"))
 local btn_2 = juce.TextButton.new(juce.String.new("Bar"))
@@ -25,30 +11,25 @@ btn_2.onClick = function() print("Button 2") end
 btn_3.onClick = function() mainComponent:repaint() end
 
 function mainComponent:paint(g, txt)
-  g:setColour(randomColor())
+  g:setColour(juce.Colours.cadetblue)
   local area = mainComponent:getLocalBounds():reduced(random:nextInt(150))
   g:fillRoundedRectangle(area:toFloat(), 5)
 end
 
 function mainComponent:resized()
-  local b = juce.RectangleInt.new(10,10,40,40)
+  local b = juce.RectangleInt.new(10, 10, 40, 40)
   btn_1:setBounds(b)
-  btn_2:setBounds(10,200,250,150)
-  btn_3:setBounds(400,200,250,150)
+  btn_2:setBounds(10, 200, 250, 150)
+  btn_3:setBounds(400, 200, 250, 150)
 end
 
-function construct(comp)
-  comp:addAndMakeVisible(mainComponent, -1)
-end
+function construct(comp) comp:addAndMakeVisible(mainComponent, -1) end
 
-function paint (comp, g)
-  g:fillAll(black)
-end
+function paint(comp, g) g:fillAll(juce.Colours.black) end
 
 function resized(comp)
   local area = comp:getLocalBounds()
   mainComponent:setBounds(area)
 end
 
-function mouseDown(comp, event)
-end
+function mouseDown(comp, event) end
