a73f4279068afc86158e35d889de6e391d0c7c61
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-11-13 01:51:44 +0100

    Add to zentrale example

--- src/scripts/zentrale.lua
+++ src/scripts/zentrale.lua
@@ -1,40 +1,103 @@
 local black = juce.Colour.new(0, 0, 0, 255)
 local grey = juce.Colour.new(53, 54, 59, 255)
 
-local function makeSidebar()
-  local this = juce.LuaComponent.new()
+local function PluginThumbnail(name)
+  local thumbnail = juce.LuaComponent.new()
+  thumbnail:setComponentID(juce.String.new(name))
 
-  local btn_1 = juce.TextButton.new(juce.String.new("Foo"))
-  local btn_2 = juce.TextButton.new(juce.String.new("Bar"))
-  local btn_3 = juce.TextButton.new(juce.String.new("Baz"))
-  this:setComponentID(juce.String.new("Sidebar"))
+  local tag1 = juce.TextButton.new(juce.String.new("tag1"))
+  local tag2 = juce.TextButton.new(juce.String.new("tag2"))
+  local tag3 = juce.TextButton.new(juce.String.new("tag3"))
 
-  btn_1:setComponentID(juce.String.new("Foo"))
-  btn_2:setComponentID(juce.String.new("Bar"))
-  btn_3:setComponentID(juce.String.new("Baz"))
+  thumbnail:addAndMakeVisible(tag1)
+  thumbnail:addAndMakeVisible(tag2)
+  thumbnail:addAndMakeVisible(tag3)
 
-  this:addAndMakeVisible(btn_1)
-  this:addAndMakeVisible(btn_2)
-  this:addAndMakeVisible(btn_3)
+  function thumbnail:paint(g)
+    g:setColour(juce.Colours.black)
+    g:fillAll()
+  end
 
-  function this:paint(g)
+  function thumbnail:resized()
+    local area = thumbnail:getLocalBounds()
+  end
+
+  return thumbnail
+end
+
+local function Sidebar()
+  local sidebar = juce.LuaComponent.new()
+
+  local plug = juce.TextButton.new(juce.String.new("Plug"))
+  local music = juce.TextButton.new(juce.String.new("Music"))
+  local rocket = juce.TextButton.new(juce.String.new("Rocket"))
+  local star = juce.TextButton.new(juce.String.new("Star"))
+  sidebar:setComponentID(juce.String.new("Sidebar"))
+
+  plug:setComponentID(juce.String.new("Plug"))
+  music:setComponentID(juce.String.new("Music"))
+  rocket:setComponentID(juce.String.new("Rocket"))
+  star:setComponentID(juce.String.new("Star"))
+
+  sidebar:addAndMakeVisible(music)
+  sidebar:addAndMakeVisible(plug)
+  sidebar:addAndMakeVisible(rocket)
+  sidebar:addAndMakeVisible(star)
+
+  function sidebar:paint(g)
     g:setColour(black)
     g:fillAll()
   end
 
-  function this:resized()
-    local area = this:getLocalBounds()
-    btn_1:setBounds(area:removeFromBottom(75):reduced(4))
-    btn_2:setBounds(area:removeFromBottom(75):reduced(4))
-    btn_3:setBounds(area:removeFromBottom(75):reduced(4))
+  function sidebar:resized()
+    local area = sidebar:getLocalBounds()
+    star:setBounds(area:removeFromBottom(75):reduced(4))
+    rocket:setBounds(area:removeFromBottom(75):reduced(4))
+    music:setBounds(area:removeFromBottom(75):reduced(4))
+    plug:setBounds(area:removeFromBottom(75):reduced(4))
   end
 
-  return this
+  return sidebar
 end
 
-local function makeContent()
+local function ContentHeader()
+  local header = juce.LuaComponent.new()
+  header:setComponentID(juce.String.new("Content Header"))
+
+  local titleText = juce.String.new("Installed Products")
+  local title = juce.Label.new(titleText, titleText)
+  title:setComponentID(titleText)
+
+  local searchText = juce.String.new("Search")
+  local search = juce.Label.new(searchText, searchText)
+  search:setComponentID(searchText)
+
+  header:addAndMakeVisible(title)
+  header:addAndMakeVisible(search)
+
+  function header:resized()
+    local area = header:getLocalBounds():reduced(16)
+    title:setBounds(area:removeFromLeft(150):toNearestInt())
+    search:setBounds(area:removeFromLeft(150):toNearestInt())
+  end
+
+  return header
+end
+
+local function MainContent()
   local content = juce.LuaComponent.new()
+  local header = ContentHeader()
+  local dynamic = PluginThumbnail("ASIC Dynamic")
+  local filter = PluginThumbnail("ASIC Filter")
+  local shape = PluginThumbnail("ASIC Shape")
+  local space = PluginThumbnail("ASIC Space")
+
   content:setComponentID(juce.String.new("Content"))
+  content:addAndMakeVisible(header)
+  content:addAndMakeVisible(dynamic)
+  content:addAndMakeVisible(filter)
+  content:addAndMakeVisible(shape)
+  content:addAndMakeVisible(space)
 
   function content:paint(g)
     g:setColour(grey)
@@ -42,21 +105,30 @@ local function makeContent()
   end
 
   function content:resized()
-    local area = content:getLocalBounds()
+    local area = content:getLocalBounds():reduced(16)
+    local height = area:getHeight() / 3
+    local width = area:getWidth() / 3
+    header:setBounds(area:removeFromTop(75):reduced(8):toNearestInt())
+
+    thumbnailArea = area:removeFromTop(200):reduced(8)
+    dynamic:setBounds(thumbnailArea:removeFromLeft(200):reduced(5))
+    filter:setBounds(thumbnailArea:removeFromLeft(200):reduced(5))
+    shape:setBounds(thumbnailArea:removeFromLeft(200):reduced(5))
+    space:setBounds(thumbnailArea:removeFromLeft(200):reduced(5))
   end
 
   return content
 end
 
-local function makeMainComponent()
+local function MainWindow()
   local mainComponent = juce.LuaComponent.new()
-  local sidebar = makeSidebar()
-  local content = makeContent()
+  local sidebar = Sidebar()
+  local content = MainContent()
 
   mainComponent:addAndMakeVisible(sidebar)
   mainComponent:addAndMakeVisible(content)
   mainComponent:setComponentID(juce.String.new("Zentrale"))
-  mainComponent:setSize(1280, 720)
+  mainComponent:setSize(1152, 648)
 
   function mainComponent:paint(g)
     g:setColour(black)
@@ -72,4 +144,4 @@ local function makeMainComponent()
   return mainComponent
 end
 
-return makeMainComponent()
+return MainWindow()
