fc5aa5bd3d78781edb7bb65416f5d978a01a8b7c
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-12-06 15:57:17 +0100

    Add OneLight-like color scheme to code editor

--- src/tool/LuaSuculenta/LuaSuculentaColors.hpp
+++ src/tool/LuaSuculenta/LuaSuculentaColors.hpp
@@ -10,4 +10,22 @@ struct LuaSuculentaColors {
     inline static juce::Colour const whiteDirt   = juce::Colour::fromRGB(240, 240, 240);
 };
 
+struct OneLightColors {
+    inline static juce::Colour const editorBackground = juce::Colour(0xFFFAFAFA);
+    inline static juce::Colour const editorForeground = juce::Colour(0xFF383A42);
+    inline static juce::Colour const editorHighlight  = juce::Colour(0xFFE5E5E6);
+    inline static juce::Colour const editorLineNumber = juce::Colour(0xFF9D9D9F);
+
+    inline static juce::Colour const codeError       = juce::Colour(0xFFFF1414);
+    inline static juce::Colour const codeKeyword     = juce::Colour(0xFFA626A4);
+    inline static juce::Colour const codeComment     = juce::Colour(0xFFA0A1A7);
+    inline static juce::Colour const codeOperator    = juce::Colour(0xFF383A42);
+    inline static juce::Colour const codeIdentifier  = juce::Colour(0xFFC18401);
+    inline static juce::Colour const codeInteger     = juce::Colour(0xFF986801);
+    inline static juce::Colour const codeFloat       = juce::Colour(0xFF986801);
+    inline static juce::Colour const codeString      = juce::Colour(0xFF50A14F);
+    inline static juce::Colour const codeBracket     = juce::Colour(0xFF383A42);
+    inline static juce::Colour const codePunctuation = juce::Colour(0xFF383A42);
+};
+
 } // namespace mc
