f2b0e2c89e867b52f96d6057ac3bcd79f2b24abf
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-12-12 21:57:05 +0100

    [runner-cli] Add component snapshot images

--- tool/jml-runner-cli/Main.cpp
+++ tool/jml-runner-cli/Main.cpp
@@ -2,13 +2,22 @@
 
 auto main(int argc, char* argv[]) -> int
 {
+    auto init = juce::ScopedJuceInitialiser_GUI{};
+
     if (argc != 2) { return EXIT_FAILURE; }
 
     auto state = sol::state{};
     state.open_libraries(sol::lib::base, sol::lib::package, sol::lib::string);
     mc::addLuaJuceModules(state);
 
-    auto script = state.load_file(argv[1]);
+    auto const scriptFile = juce::File{argv[1]};
+    if (not scriptFile.existsAsFile()) {
+        std::cerr << "Script file does not exist: " << argv[1] << '\n';
+        return EXIT_FAILURE;
+    }
+
+    scriptFile.getParentDirectory().setAsCurrentWorkingDirectory();
+    auto script = state.load_file(scriptFile.getFullPathName().toStdString());
     if (not script.valid()) {
         sol::error error = script;
         std::cerr << "Load error:" << error.what() << '\n';
@@ -22,6 +31,23 @@ auto main(int argc, char* argv[]) -> int
         std::cerr << "Run error:" << error.what() << '\n';
         return EXIT_FAILURE;
     }
+
+    sol::object obj = result;
+    auto* component = obj.as<juce::Component*>();
+    if (component == nullptr) {
+        std::cerr << "Failed to get juce::Component* from lua result\n";
+        return EXIT_FAILURE;
+    }
+
+    component->resized();
+    auto snapshot = component->createComponentSnapshot(component->getBounds());
+
+    auto snapShotFile = juce::File{R"(C:\Developer\moderncircuits\tests\juce-lua\pic.png)"};
+    if (snapShotFile.existsAsFile()) { snapShotFile.deleteFile(); }
+    auto out = snapShotFile.createOutputStream();
+
+    auto jpg = juce::PNGImageFormat{};
+    jpg.writeImageToStream(snapshot, *out);
 
     return EXIT_SUCCESS;
 }
