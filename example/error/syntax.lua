68f77361888e272364c0de73b26c827b77dd942e
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-12-12 21:39:40 +0100

    [runner-cli] Add tool

---
+++ example/error/syntax.lua
@@ -0,0 +1,7 @@
+local function MainComponent()
+    local mainComponent = juce.Component.new(
+    mainComponent:setSize(640, 480)
+    return mainComponent
+end
+
+return MainComponent()
--- tool/jml-runner-cli/Main.cpp
+++ tool/jml-runner-cli/Main.cpp
@@ -2,6 +2,26 @@
 
 auto main(int argc, char* argv[]) -> int
 {
-    juce::ignoreUnused(argc, argv);
-    return 0;
+    if (argc != 2) { return EXIT_FAILURE; }
+
+    auto state = sol::state{};
+    state.open_libraries(sol::lib::base, sol::lib::package, sol::lib::string);
+    mc::addLuaJuceModules(state);
+
+    auto script = state.load_file(argv[1]);
+    if (not script.valid()) {
+        sol::error error = script;
+        std::cerr << "Load error:" << error.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    auto factory = script.get<sol::protected_function>();
+    auto result  = factory();
+    if (not result.valid()) {
+        sol::error error = result;
+        std::cerr << "Run error:" << error.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
